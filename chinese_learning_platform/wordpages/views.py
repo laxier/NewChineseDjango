959bc029919780929b9f841af3c70f999b9b6906
Кирилл Т <hash0xcb8c@yandex.ru>
2024-10-19 15:35:52 +0300

    feat: Add word info when clicking on items in the mind map
    - Implemented modals for adding, editing, and deleting sentences.
    - Updated the sentence view to support the different template
    - Users can now manage sentences directly from the word details page in the mind map.

--- chinese_learning_platform/wordpages/views.py
+++ chinese_learning_platform/wordpages/views.py
@@ -3,13 +3,21 @@ from .models import RelatedWord, Sentence
 from chineseword.models import ChineseWord
 from frontend.views import CurrentUserMixin
 
+
 class ChineseWordDetailView(CurrentUserMixin, generic.DetailView):
     model = ChineseWord
-    template_name = 'wordpages/chinese_word_detail.html'  # Adjust the template path
     context_object_name = 'chinese_word'
+
+    def get_template_names(self):
+        is_mindmap = self.request.GET.get('is_mindmap', 'false') == 'true'
+        if is_mindmap:
+            return ['wordpages/chinese_word_detail_no_header.html']
+        return ['wordpages/chinese_word_detail.html']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['sentences'] = Sentence.objects.filter(chinese_word=self.object)
         context['related_words'] = RelatedWord.objects.filter(word=self.object)
+        context['is_favorite'] = self.request.user in self.object.favorites.all()
+
         return context
