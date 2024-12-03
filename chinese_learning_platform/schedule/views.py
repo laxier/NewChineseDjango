d67b221448dc03b1249917faa9d9549bb62aac95
Кирилл Т <hash0xcb8c@yandex.ru>
2024-12-03 06:01:22 +0300

    feat(schedule): implement LessonListView and HomeworkListView with user-specific filtering

--- chinese_learning_platform/schedule/views.py
+++ chinese_learning_platform/schedule/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Lesson, Homework
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class LessonListView(LoginRequiredMixin, ListView):
+    model = Lesson
+    template_name = "schedule/lesson_list.html"
+    context_object_name = "lessons"
+
+    def get_queryset(self):
+        return Lesson.objects.filter(user=self.request.user).order_by('date')
+
+
+class HomeworkListView(LoginRequiredMixin, ListView):
+    model = Homework
+    template_name = "schedule/homework_list.html"
+    context_object_name = "homeworks"
+
+    def get_queryset(self):
+        lesson = get_object_or_404(Lesson, pk=self.kwargs['pk'], user=self.request.user)
+        return Homework.objects.filter(lesson=lesson).order_by('due_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lesson'] = get_object_or_404(Lesson, pk=self.kwargs['pk'], user=self.request.user)
+        return context
