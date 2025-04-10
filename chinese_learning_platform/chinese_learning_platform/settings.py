468ab2d829f0a2acd1d6c356d3c811f63336e033
Кирилл Т <hash0xcb8c@yandex.ru>
2025-04-10 16:10:43 +0300

    Move SQLite database to 'data' directory one level above BASE_DIR

--- chinese_learning_platform/chinese_learning_platform/settings.py
+++ chinese_learning_platform/chinese_learning_platform/settings.py
@@ -133,7 +133,7 @@ WSGI_APPLICATION = 'chinese_learning_platform.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': (BASE_DIR.parent / 'data' / 'db.sqlite3').resolve(),
     }
 }
 
