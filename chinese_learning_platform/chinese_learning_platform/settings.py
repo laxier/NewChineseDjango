db0ab5a312e0ab6dd49991a954f719b9aa9959e7
Кирилл Т <hash0xcb8c@yandex.ru>
2025-06-21 17:08:42 +0300

    committed

--- chinese_learning_platform/chinese_learning_platform/settings.py
+++ chinese_learning_platform/chinese_learning_platform/settings.py
@@ -171,7 +171,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
     BASE_DIR.parent / 'data' / 'static',
 ]
 STATIC_ROOT = BASE_DIR.parent / 'data' / 'staticfiles'
