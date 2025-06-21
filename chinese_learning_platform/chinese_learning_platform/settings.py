282b67ac39dfe886cf366a83b96406c14dc0e3ce
Кирилл Т <hash0xcb8c@yandex.ru>
2025-06-21 17:06:49 +0300

    Add dialogue text splitting and styling for LexicalExercises component

--- chinese_learning_platform/chinese_learning_platform/settings.py
+++ chinese_learning_platform/chinese_learning_platform/settings.py
@@ -169,16 +169,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static',
+    BASE_DIR.parent / 'data' / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR.parent / 'data' / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.parent / 'data' / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
